4f1bda94a50f9baf1ac65d58452c256426cb7cf4
Alexis Jaramillo <alexisj1881@gmail.com>
2026-06-18 19:45:39 -0500

    Actualizar SDK Mercado Pago

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -1,43 +1,45 @@
-const mercadopago = require("mercadopago");
+ const { MercadoPagoConfig, Preference } = require("mercadopago");
 
-mercadopago.configure({
-  access_token: process.env.MP_ACCESS_TOKEN,
+const client = new MercadoPagoConfig({
+  accessToken: process.env.MP_ACCESS_TOKEN
 });
 
 exports.handler = async () => {
   try {
-    const preference = {
-      items: [
-        {
-          title: "Ni Frío Ni Fuego",
-          quantity: 1,
-          currency_id: "COP",
-          unit_price: 49000
-        }
-      ],
+    const preference = new Preference(client);
 
-      back_urls: {
-        success: "https://nifrionifuego.netlify.app/gracias",
-        failure: "https://nifrionifuego.netlify.app/",
-        pending: "https://nifrionifuego.netlify.app/"
-      },
-
-      auto_return: "approved"
-    };
-
-    const response = await mercadopago.preferences.create(preference);
+    const response = await preference.create({
+      body: {
+        items: [
+          {
+            title: "Ni Frío Ni Fuego",
+            quantity: 1,
+            currency_id: "COP",
+            unit_price: 49000
+          }
+        ],
+        back_urls: {
+          success: "https://nifrionifuego.netlify.app/gracias",
+          failure: "https://nifrionifuego.netlify.app",
+          pending: "https://nifrionifuego.netlify.app"
+        },
+        auto_return: "approved"
+      }
+    });
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        init_point: response.body.init_point
+        init_point: response.init_point
       })
     };
 
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify(error)
+      body: JSON.stringify({
+        error: error.message
+      })
     };
   }
 };
